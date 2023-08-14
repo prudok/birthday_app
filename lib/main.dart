243af8c2923c05d8a0f2cb3059926c54bd887025
Roman Prudchenko <romanprudchen@gmail.com>
2023-08-14 19:32:34 +0300

    chore: add github actions

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:birthday_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runZonedGuarded(() async {
+void main() => runZonedGuarded<void>(() async {
       WidgetsFlutterBinding.ensureInitialized();
       await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:birthday_app/features/guests/presentation/widgets/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Url widget has a url', (tester) async {
+    await tester.pumpWidget(const GuestForm());
+    final guestInitName = find.text('');
+    final guestInitSurname = find.text('');
+
+    expect(guestInitName, findsOneWidget);
+    expect(guestInitSurname, findsOneWidget);
+  });
+}
